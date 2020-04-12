573b5fb6ee73c072b63c1a8c540dbfc3e5d5e3cb
V <Pablo_Mayer@outlook.de>
2020-04-12 17:15:16 +0200

    Made the Index.html
    Created a basic index.html with Javascript and external CSS as well as the socket connection

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+var express = require('express');
+    app = express();
+    serv = require('http').Server(app);
+    path = require('path')
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+app.use(express.static(path.join(__dirname, 'public')));
+serv.listen(2000);
+
+console.log('Server started.');
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket) {
+    console.log('socket connection');
+
+    socket.on('happy', function(){
+        console.log('happy');
+    });
+
+    socket.emit('eegg', {msg:'You found the Easteregg!'});
+});
+
