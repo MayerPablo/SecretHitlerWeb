b34338ed06d7e1f3830c183bfe10320dcac62270
V <Pablo_Mayer@outlook.de>
2020-04-13 22:49:20 +0200

    SVG and Animation
    Switched to .svg files, still need to change them, added attribute role to localStorage (doesn't work right now)
    Added Animation for Articles.

--- client/js/game.js
+++ client/js/game.js
@@ -1,1 +1,15 @@
 document.getElementById('player_name').innerHTML = localStorage.name;
+if(localStorage.role == 0) {
+    document.getElementById('player_role').innerHTML = "Liberal";
+} else if(localStorage.role == 'fascist') {
+    document.getElementById('player_role').innerHTML = "Fascist";
+} else {
+    document.getElementById('player_role').innerHTML = "Hitler";
+}
+window.onclick = function() {
+    document.getElementById('lib_policy_one').style.display = "block";
+    document.getElementById('lib_policy_one').style.transitionDuration = "400ms";
+    document.getElementById('lib_policy_one').style.transform = "translate(0vw, 12.6vw)";
+    document.getElementById('lib_policy_one').style.opacity = 1;
+
+};
--- client/js/index.js
+++ client/js/index.js
@@ -1,20 +1,21 @@
 var socket = io();
 var player_name = '';
-document.getElementById('play_button').onclick = function(){
+var player_role = 'liberal';
+document.getElementById('play_button').onclick = function() {
     player_name = document.getElementById("input_name").value;
-    if(player_name == ''){
+    if(player_name == '') {
         document.getElementById('display_name').style.color = 'rgb(139, 9, 9)'
         document.getElementById("display_name").innerHTML = 'Enter a name';
         document.getElementById('input_name').style.borderColor = 'rgb(139, 9, 9)'
-    } else if(player_name == 'Ziddmaster'){   
+    } else if(player_name == 'Ziddmaster') {   
         document.getElementById('display_name').style.color = 'rgb(139, 9, 9)'
         document.getElementById("display_name").innerHTML = "The name " + player_name + " is banned";
         document.getElementById('input_name').style.borderColor = 'rgb(139, 9, 9)'
-    } else{
+    } else {
     document.getElementById('display_name').style.color = '#5A9BA6'
     document.getElementById('input_name').style.borderColor = '#5A9BA6'
-    document.getElementById("display_name").innerHTML = player_name;
     localStorage.setItem("name", player_name);
+    localStorage.setItem("role", player_role);
     socket.emit(player_name);
     location.href='client/game.html'
     }
